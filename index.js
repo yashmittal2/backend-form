f35998974eb1dcd8bb39445bae5eab8a039f6fd2
yashmittal2 <yashmittal56789@gmail.com>
2023-08-01 12:14:47 +0530

    new edit

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const server = express();
-server.use(cors({ origin: "https://timely-tiramisu-9a5114.netlify.app" }));
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
